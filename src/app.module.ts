bcc32e70929fe9a4bb6a1b685f865d21c81777ca
Jrm1992 <jose_roberto_6@msn.com>
2023-10-05 19:19:08 -0300

    feat(email): created email controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailService } from './email/email.service';
+import { EmailController } from './email/email.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, EmailController],
   providers: [AppService, EmailService],
 })
 export class AppModule {}
