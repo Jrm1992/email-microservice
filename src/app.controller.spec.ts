7b0260153eea96bc0842e6635e9345ec44b3ac73
Jrm1992 <jose_roberto_6@msn.com>
2023-10-10 01:58:54 -0300

    refactor: remove hello world from appController

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +7,20 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should rredirect to /api', () => {
+      const mockResponse = {
+        redirect: jest.fn(),
+      };
+
+      appController.redirect(mockResponse);
+
+      expect(mockResponse.redirect).toHaveBeenCalledWith('/api');
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EmailController } from './use-cases/email/email.controller';
 import { EmailService } from './use-cases/email/email.service';
 import { CoreModule } from './core/core.module';
@@ -9,6 +8,6 @@ import { CoreModule } from './core/core.module';
 @Module({
   imports: [ConfigModule.forRoot(), CoreModule],
   controllers: [AppController, EmailController],
-  providers: [AppService, EmailService],
+  providers: [EmailService],
 })
 export class AppModule {}
