9a5084092b786db104d2973c23bf7bd631f8ebe7
Jrm1992 <jose_roberto_6@msn.com>
2023-10-09 23:06:46 -0300

    feat(use-cases): move email to use-cases

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EmailController } from './email/email.controller';
-import { EmailService } from './email/email.service';
+import { EmailController } from './use-cases/email/email.controller';
+import { EmailService } from './use-cases/email/email.service';
 import { CoreModule } from './core/core.module';
 
 @Module({
