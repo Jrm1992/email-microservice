3acc34ab982d4a570d92c6e0bc716b53e0155f2d
Jrm1992 <jose_roberto_6@msn.com>
2023-10-09 23:04:59 -0300

    feat(code): created module core

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailController } from './email/email.controller';
 import { EmailService } from './email/email.service';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), CoreModule],
   controllers: [AppController, EmailController],
   providers: [AppService, EmailService],
 })
